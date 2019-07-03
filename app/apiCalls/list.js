73f7142a2d83c54289dbae3e51f71f3b888ba463
Vu PHAM <phamhoaivu911@gmail.com>
2019-07-03 15:19:03 +0700

    setup enzyme

---
+++ app/apiCalls/list.js
@@ -0,0 +1,2 @@
+const url =
+  'https://openlibrary.org/api/books?bibkeys=ISBN:0385472579,LCCN:62019420,ISBN:9780980200447,ISBN:978-1-60309-036-0,ISBN:978-1-60309-089-6,ISBN:978-1-60309-410-8&format=json&jscmd=data';
